2fd479b9343ee4ca1526eba279bbb0e12dbb5e1b
JonnyHightower <jonnyhightower@funkygeek.com>
2016-11-21 16:59:12 +0000

    Added winexe and openvas-nasl

--- install.sh
+++ install.sh
@@ -22,21 +22,17 @@ if [ ! -z $INVOKEDBYNEETUPDATE ] && [ $INVOKEDBYNEETUPDATE -eq 1 ]; then
 
 	Install sbg SBG
 	if [ $? -eq 0 ]; then
-		#echo "sbg=${NEET}/pkg/bin/sbg" >> "${CONFDIR}/locations"
 		newLocation sbg "${NEET}/pkg/bin/sbg"
 	fi
 
 	Install tnsenum TNSEnum
 	if [ $? -eq 0 ]; then
-		#echo "tnsenum=${NEET}/pkg/bin/tnsenum" >> "${CONFDIR}/locations"
 		newLocation tnsenum "${NEET}/pkg/bin/tnsenum"
 	fi
 
 	Install libesedb libesedb
 	if [ $? -eq 0 ]; then
-		#echo "esedbexport=${NEET}/pkg/bin/esedbexport" >> "${CONFDIR}/locations"
 		newLocation esedbexport "${NEET}/pkg/bin/esedbexport"
-		# Now do ntdsxtract
 		PkgInstall ntdsxtract ntdsxtract
 	fi
 
@@ -45,19 +41,13 @@ if [ ! -z $INVOKEDBYNEETUPDATE ] && [ $INVOKEDBYNEETUPDATE -eq 1 ]; then
 	newLocation lsadump "${NEET}/pkg/creddump/lsadump.py"
 	newLocation cachedump "${NEET}/pkg/creddump/cachedump.py"
 
-	#echo "pwdump=${NEET}/pkg/creddump/pwdump.py " >> "${CONFDIR}/locations"
-	#echo "lsadump=${NEET}/pkg/creddump/lsadump.py " >> "${CONFDIR}/locations"
-	#echo "cachedump=${NEET}/pkg/creddump/cachedump.py " >> "${CONFDIR}/locations"
-
 	if ! type amap >/dev/null 2>&1; then
 		Install amap "THC Amap"
 		if [ $? -eq 0 ]; then
-			#echo "amap=${NEET}/pkg/bin/amap " >> "${CONFDIR}/locations"
 			newLocation amap "${NEET}/pkg/bin/amap"
 		fi
 	else
 		OV=`type amap | awk {print'$3'}`
-		#echo "amap=$OV" >> "${CONFDIR}/locations"
 		newLocation amap "$OV"
 	fi
 
@@ -65,15 +55,28 @@ if [ ! -z $INVOKEDBYNEETUPDATE ] && [ $INVOKEDBYNEETUPDATE -eq 1 ]; then
 
 	Install on On
 	if [ $? -eq 0 ]; then
-		#echo "on=${NEET}/pkg/bin/on" >> "${CONFDIR}/locations"
 		newLocation on "${NEET}/pkg/bin/on"
 	fi
 
 	PkgInstall framework2 "Metasploit_Framework 2"
 
 	Install dirb221 "DIRB_Directory_Brute_Force"
-	#echo "dirb=${NEET}/pkg/bin/dirb " >> "${CONFDIR}/locations"
 	newLocation dirb "${NEET}/pkg/bin/dirb"
+
+	if ! systemHas openvas-nasl; then
+		Install openvas-libraries-8.0.8 "OpenVAS_NASL"	
+		if [ $? -eq 0 ]; then
+			newLocation openvas-nasl "${NEET}/pkg/bin/openvas-nasl"
+		fi
+	fi
+
+	if ! systemHas winexe; then
+		Install winexe-1.00 "Winexe"
+		if [ $? -eq 0 ]; then
+			newLocation winexe "${NEET}/pkg/bin/winexe"
+		fi
+	fi
+
 
 	# Perl dependencies
 	cd "${INST}/content/" && ./install_perl.sh
